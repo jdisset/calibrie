51e2a1f98f556e53c98beeb0ef23b427e90c40ae
Jean Disset <j.disset@gmail.com>
2023-03-15 15:14:55 -0400

    Add package setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calibry",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Calibry: flow cytometry calibration package",
+    author='Jean Disset',
+    author_email='jdisset@mit.edu',
+)
